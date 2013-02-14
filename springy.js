3851d95ff57907c823ea0a893887978c731fa16b
George <gnthackray@btinternet.com>
2013-02-14 09:11:43 -0500

    on going refactoring

--- springy.js
+++ springy.js
@@ -25,11 +25,13 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
+var Vector;
+
 //https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/forEach
 if ( !Array.prototype.forEach ) {
   Array.prototype.forEach = function( callback, thisArg ) {
     var T, k;
-    if ( this == null ) {
+    if ( this === null ) {
       throw new TypeError( " this is null or not defined" );
     }
     var O = Object(this);
@@ -80,166 +82,166 @@ var Graph = function() {
 Graph.prototype = {
 
     addNode: function (node) {
-	    if (typeof(this.nodeSet[node.id]) === 'undefined') {
-		    this.nodes.push(node);
-	    }
-
-	    this.nodeSet[node.id] = node;
-
-	    this.notify();
-	    return node;
+        if (typeof(this.nodeSet[node.id]) === 'undefined') {
+            this.nodes.push(node);
+        }
+        
+        this.nodeSet[node.id] = node;
+        
+        this.notify();
+        return node;
     },
     addEdge:function(edge) {
-	    var exists = false;
-	    this.edges.forEach(function(e) {
-		    if (edge.id === e.id) { exists = true; }
-	    });
-
-	    if (!exists) {
-		    this.edges.push(edge);
-	    }
-
-	    if (typeof(this.adjacency[edge.source.id]) === 'undefined') {
-		    this.adjacency[edge.source.id] = {};
-	    }
-	    if (typeof(this.adjacency[edge.source.id][edge.target.id]) === 'undefined') {
-		    this.adjacency[edge.source.id][edge.target.id] = [];
-	    }
-
-	    exists = false;
-	    this.adjacency[edge.source.id][edge.target.id].forEach(function(e) {
-			    if (edge.id === e.id) { exists = true; }
-	    });
-
-	    if (!exists) {
-		    this.adjacency[edge.source.id][edge.target.id].push(edge);
-	    }
-
-	    this.notify();
-	    return edge;
+        var exists = false;
+        this.edges.forEach(function(e) {
+            if (edge.id === e.id) { exists = true; }
+        });
+        
+        if (!exists) {
+            this.edges.push(edge);
+        }
+        
+        if (typeof(this.adjacency[edge.source.id]) === 'undefined') {
+            this.adjacency[edge.source.id] = {};
+        }
+        if (typeof(this.adjacency[edge.source.id][edge.target.id]) === 'undefined') {
+            this.adjacency[edge.source.id][edge.target.id] = [];
+        }
+        
+        exists = false;
+        this.adjacency[edge.source.id][edge.target.id].forEach(function(e) {
+            if (edge.id === e.id) { exists = true; }
+        });
+        
+        if (!exists) {
+            this.adjacency[edge.source.id][edge.target.id].push(edge);
+        }
+        
+        this.notify();
+        return edge;
     },
     newNode: function(data) {
-	    var node = new Node(this.nextNodeId++, data);
-	    this.addNode(node);
-	    return node;
+        var node = new Node(this.nextNodeId++, data);
+        this.addNode(node);
+        return node;
     },
     newEdge: function(source, target, data) {
-	    var edge = new Edge(this.nextEdgeId++, source, target, data);
-	    this.addEdge(edge);
-	    return edge;
+        var edge = new Edge(this.nextEdgeId++, source, target, data);
+        this.addEdge(edge);
+        return edge;
 	},
 	// find the edges from node1 to node2
     getEdges:function(node1, node2) {
-	    if (typeof(this.adjacency[node1.id]) !== 'undefined'
-		    && typeof(this.adjacency[node1.id][node2.id]) !== 'undefined') {
-		    return this.adjacency[node1.id][node2.id];
-	    }
-	    return [];
+        if (typeof(this.adjacency[node1.id]) !== 'undefined'
+            && typeof(this.adjacency[node1.id][node2.id]) !== 'undefined') {
+            return this.adjacency[node1.id][node2.id];
+        }
+        return [];
 	},
 	// remove a node and it's associated edges from the graph
     removeNode: function(node) {
-	    if (typeof(this.nodeSet[node.id]) !== 'undefined') {
-		    delete this.nodeSet[node.id];
-	    }
-
-	    for (var i = this.nodes.length - 1; i >= 0; i--) {
-		    if (this.nodes[i].id === node.id) {
-			    this.nodes.splice(i, 1);
-		    }
-	    }
-	    this.detachNode(node);
+        if (typeof(this.nodeSet[node.id]) !== 'undefined') {
+            delete this.nodeSet[node.id];
+        }
+        
+        for (var i = this.nodes.length - 1; i >= 0; i--) {
+            if (this.nodes[i].id === node.id) {
+                this.nodes.splice(i, 1);
+            }
+        }
+        this.detachNode(node);
 	},
 	// removes edges associated with a given node
     detachNode:function(node) {
-	    var tmpEdges = this.edges.slice();
-	    tmpEdges.forEach(function(e) {
-		    if (e.source.id === node.id || e.target.id === node.id) {
-			    this.removeEdge(e);
-		    }
-	    }, this);
-
-	    this.notify();
+        var tmpEdges = this.edges.slice();
+        tmpEdges.forEach(function(e) {
+            if (e.source.id === node.id || e.target.id === node.id) {
+                this.removeEdge(e);
+            }
+        }, this);
+        
+        this.notify();
 	},
 	// remove a node and it's associated edges from the graph
     removeEdge : function(edge) {
-	    for (var i = this.edges.length - 1; i >= 0; i--) {
-		    if (this.edges[i].id === edge.id) {
-			    this.edges.splice(i, 1);
-		    }
-	    }
+        for (var i = this.edges.length - 1; i >= 0; i--) {
+            if (this.edges[i].id === edge.id) {
+                this.edges.splice(i, 1);
+            }
+        }
+        
+        for (var x in this.adjacency) {
+            for (var y in this.adjacency[x]) {
+                var edges = this.adjacency[x][y];
+                
+                for (var j=edges.length - 1; j>=0; j--) {
+                    if (this.adjacency[x][y][j].id === edge.id) {
+                        this.adjacency[x][y].splice(j, 1);
+                    }
+                }
+            }
+        }
 
-	    for (var x in this.adjacency) {
-		    for (var y in this.adjacency[x]) {
-			    var edges = this.adjacency[x][y];
-
-			    for (var j=edges.length - 1; j>=0; j--) {
-				    if (this.adjacency[x][y][j].id === edge.id) {
-					    this.adjacency[x][y].splice(j, 1);
-				    }
-			    }
-		    }
-	    }
-
-	    this.notify();
+        this.notify();
     },
     merge : function (data) {
 
         /* Merge a list of nodes and edges into the current graph. eg.
         var o = {
-	        nodes: [
-		        {id: 123, data: {type: 'user', userid: 123, displayname: 'aaa'}},
-		        {id: 234, data: {type: 'user', userid: 234, displayname: 'bbb'}}
-	        ],
-	        edges: [
-		        {from: 0, to: 1, type: 'submitted_design', directed: true, data: {weight: }}
-	        ]
+            nodes: [
+                {id: 123, data: {type: 'user', userid: 123, displayname: 'aaa'}},
+                {id: 234, data: {type: 'user', userid: 234, displayname: 'bbb'}}
+            ],
+            edges: [
+                {from: 0, to: 1, type: 'submitted_design', directed: true, data: {weight: }}
+            ]
         }
         */
-	    var nodes = [];
-	    data.nodes.forEach(function(n) {
-		    nodes.push(this.addNode(new Node(n.id, n.data)));
-	    }, this);
+        var nodes = [];
+            data.nodes.forEach(function(n) {
+            nodes.push(this.addNode(new Node(n.id, n.data)));
+        }, this);
 
-	    data.edges.forEach(function(e) {
-		    var from = nodes[e.from];
-		    var to = nodes[e.to];
-
-		    var id = (e.directed)
-			    ? (id = e.type + "-" + from.id + "-" + to.id)
-			    : (from.id < to.id) // normalise id for non-directed edges
-				    ? e.type + "-" + from.id + "-" + to.id
-				    : e.type + "-" + to.id + "-" + from.id;
-
-		    var edge = this.addEdge(new Edge(id, from, to, e.data));
-		    edge.data.type = e.type;
-	    }, this);
+        data.edges.forEach(function(e) {
+            var from = nodes[e.from];
+            var to = nodes[e.to];
+            
+            var id = (e.directed)
+            ? (id = e.type + "-" + from.id + "-" + to.id)
+            : (from.id < to.id) // normalise id for non-directed edges
+            ? e.type + "-" + from.id + "-" + to.id
+            : e.type + "-" + to.id + "-" + from.id;
+            
+            var edge = this.addEdge(new Edge(id, from, to, e.data));
+            edge.data.type = e.type;
+        }, this);
     },
     filterNodes : function(fn) {
-	    var tmpNodes = this.nodes.slice();
-	    tmpNodes.forEach(function(n) {
-		    if (!fn(n)) {
-			    this.removeNode(n);
-		    }
-	    }, this);
+        var tmpNodes = this.nodes.slice();
+        tmpNodes.forEach(function(n) {
+            if (!fn(n)) {
+                this.removeNode(n);
+            }
+        }, this);
     },
     filterEdges:function(fn) {
-	    var tmpEdges = this.edges.slice();
-	    tmpEdges.forEach(function(e) {
-		    if (!fn(e)) {
-			    this.removeEdge(e);
-		    }
-	    }, this);
+        var tmpEdges = this.edges.slice();
+            tmpEdges.forEach(function(e) {
+            if (!fn(e)) {
+                this.removeEdge(e);
+            }
+        }, this);
     },
     addGraphListener :function(obj) {
-	    this.eventListeners.push(obj);
+        this.eventListeners.push(obj);
     },
     notify :function() {
-	    this.eventListeners.forEach(function(obj){
-		    obj.graphChanged();
-	    });
+        this.eventListeners.forEach(function(obj){
+            obj.graphChanged();
+        });
     }
 
-}
+};
  
 
 
@@ -260,148 +262,141 @@ Layout.ForceDirected = function(graph, stiffness, repulsion, damping) {
 	this.edgeSprings = {}; // keep track of springs associated with edges
 };
 
+Layout.ForceDirected.prototype = {
+    point: function(node) {
+        if (typeof(this.nodePoints[node.id]) === 'undefined') {
+            var mass = typeof(node.data.mass) !== 'undefined' ? node.data.mass : 1.0;
+            this.nodePoints[node.id] = new Layout.ForceDirected.Point(Vector.random(), mass);
+        }
+        
+        return this.nodePoints[node.id];
+    },
 
+    spring:function(edge) {
+        if (typeof(this.edgeSprings[edge.id]) === 'undefined') {
+            var length = typeof(edge.data.length) !== 'undefined' ? edge.data.length : 1.0;
+            
+            var existingSpring = false;
+            
+            var from = this.graph.getEdges(edge.source, edge.target);
+            from.forEach(function(e) {
+                if (existingSpring === false && typeof(this.edgeSprings[e.id]) !== 'undefined') {
+                    existingSpring = this.edgeSprings[e.id];
+                }
+            }, this);
+            
+            if (existingSpring !== false) {
+                return new Layout.ForceDirected.Spring(existingSpring.point1, existingSpring.point2, 0.0, 0.0);
+            }
+            
+            var to = this.graph.getEdges(edge.target, edge.source);
+            from.forEach(function(e){
+                if (existingSpring === false && typeof(this.edgeSprings[e.id]) !== 'undefined') {
+                    existingSpring = this.edgeSprings[e.id];
+                }
+            }, this);
+            
+            if (existingSpring !== false) {
+                 return new Layout.ForceDirected.Spring(existingSpring.point2, existingSpring.point1, 0.0, 0.0);
+            }
+            
+            this.edgeSprings[edge.id] = new Layout.ForceDirected.Spring(
+            this.point(edge.source), this.point(edge.target), length, this.stiffness
+            );
+        }
+        
+        return this.edgeSprings[edge.id];
+    },
+    // callback should accept two arguments: Node, Point
+    eachNode:function(callback) {
+        var t = this;
+        this.graph.nodes.forEach(function(n){
+            callback.call(t, n, t.point(n));
+        });
+    },
 
+    // callback should accept two arguments: Edge, Spring
+    eachEdge:function(callback) {
+        var t = this;
+        this.graph.edges.forEach(function(e){
+            callback.call(t, e, t.spring(e));
+        });
+    },
 
+    // callback should accept one argument: Spring
+    eachSpring: function(callback) {
+        var t = this;
+        this.graph.edges.forEach(function(e){
+            callback.call(t, t.spring(e));
+        });
+    },
 
+    // Physics stuff
+    applyCoulombsLaw:function() {
+        this.eachNode(function(n1, point1) {
+            this.eachNode(function(n2, point2) {
+                if (point1 !== point2)
+                {
+                    var d = point1.p.subtract(point2.p);
+                    var distance = d.magnitude() + 0.1; // avoid massive forces at small distances (and divide by zero)
+                    var direction = d.normalise();
+                    
+                    // apply force to each end point
+                    point1.applyForce(direction.multiply(this.repulsion).divide(distance * distance * 0.5));
+                    point2.applyForce(direction.multiply(this.repulsion).divide(distance * distance * -0.5));
+                }
+            });
+        });
+    },
 
+    applyHookesLaw: function() {
+        this.eachSpring(function(spring){
+            var d = spring.point2.p.subtract(spring.point1.p); // the direction of the spring
+            var displacement = spring.length - d.magnitude();
+            var direction = d.normalise();
+            
+            // apply force to each end point
+            spring.point1.applyForce(direction.multiply(spring.k * displacement * -0.5));
+            spring.point2.applyForce(direction.multiply(spring.k * displacement * 0.5));
+        });
+    },
 
-Layout.ForceDirected.prototype.point = function(node) {
-	if (typeof(this.nodePoints[node.id]) === 'undefined') {
-		var mass = typeof(node.data.mass) !== 'undefined' ? node.data.mass : 1.0;
-		this.nodePoints[node.id] = new Layout.ForceDirected.Point(Vector.random(), mass);
-	}
+    attractToCentre:function() {
+        this.eachNode(function(node, point) {
+            var direction = point.p.multiply(-1.0);
+            point.applyForce(direction.multiply(this.repulsion / 50.0));
+        });
+    },
+    updateVelocity: function(timestep) {
+        this.eachNode(function(node, point) {
+            // Is this, along with updatePosition below, the only places that your
+            // integration code exist?
+            point.v = point.v.add(point.a.multiply(timestep)).multiply(this.damping);
+            point.a = new Vector(0,0);
+        });
+    },
 
-	return this.nodePoints[node.id];
+    updatePosition:function(timestep) {
+        this.eachNode(function(node, point) {
+            // Same question as above; along with updateVelocity, is this all of
+            // your integration code?
+            point.p = point.p.add(point.v.multiply(timestep));
+        });
+    },
+
+    // Calculate the total kinetic energy of the system
+    totalEnergy :function(timestep) {
+        var energy = 0.0;
+        this.eachNode(function(node, point) {
+            var speed = point.v.magnitude();
+            energy += 0.5 * point.m * speed * speed;
+        });
+        
+        return energy;
+    }
+    
+        
 };
-
-Layout.ForceDirected.prototype.spring = function(edge) {
-	if (typeof(this.edgeSprings[edge.id]) === 'undefined') {
-		var length = typeof(edge.data.length) !== 'undefined' ? edge.data.length : 1.0;
-
-		var existingSpring = false;
-
-		var from = this.graph.getEdges(edge.source, edge.target);
-		from.forEach(function(e) {
-			if (existingSpring === false && typeof(this.edgeSprings[e.id]) !== 'undefined') {
-				existingSpring = this.edgeSprings[e.id];
-			}
-		}, this);
-
-		if (existingSpring !== false) {
-			return new Layout.ForceDirected.Spring(existingSpring.point1, existingSpring.point2, 0.0, 0.0);
-		}
-
-		var to = this.graph.getEdges(edge.target, edge.source);
-		from.forEach(function(e){
-			if (existingSpring === false && typeof(this.edgeSprings[e.id]) !== 'undefined') {
-				existingSpring = this.edgeSprings[e.id];
-			}
-		}, this);
-
-		if (existingSpring !== false) {
-			return new Layout.ForceDirected.Spring(existingSpring.point2, existingSpring.point1, 0.0, 0.0);
-		}
-
-		this.edgeSprings[edge.id] = new Layout.ForceDirected.Spring(
-			this.point(edge.source), this.point(edge.target), length, this.stiffness
-		);
-	}
-
-	return this.edgeSprings[edge.id];
-};
-
-// callback should accept two arguments: Node, Point
-Layout.ForceDirected.prototype.eachNode = function(callback) {
-	var t = this;
-	this.graph.nodes.forEach(function(n){
-		callback.call(t, n, t.point(n));
-	});
-};
-
-// callback should accept two arguments: Edge, Spring
-Layout.ForceDirected.prototype.eachEdge = function(callback) {
-	var t = this;
-	this.graph.edges.forEach(function(e){
-		callback.call(t, e, t.spring(e));
-	});
-};
-
-// callback should accept one argument: Spring
-Layout.ForceDirected.prototype.eachSpring = function(callback) {
-	var t = this;
-	this.graph.edges.forEach(function(e){
-		callback.call(t, t.spring(e));
-	});
-};
-
-// Physics stuff
-Layout.ForceDirected.prototype.applyCoulombsLaw = function() {
-	this.eachNode(function(n1, point1) {
-		this.eachNode(function(n2, point2) {
-			if (point1 !== point2)
-			{
-				var d = point1.p.subtract(point2.p);
-				var distance = d.magnitude() + 0.1; // avoid massive forces at small distances (and divide by zero)
-				var direction = d.normalise();
-
-				// apply force to each end point
-				point1.applyForce(direction.multiply(this.repulsion).divide(distance * distance * 0.5));
-				point2.applyForce(direction.multiply(this.repulsion).divide(distance * distance * -0.5));
-			}
-		});
-	});
-};
-
-Layout.ForceDirected.prototype.applyHookesLaw = function() {
-	this.eachSpring(function(spring){
-		var d = spring.point2.p.subtract(spring.point1.p); // the direction of the spring
-		var displacement = spring.length - d.magnitude();
-		var direction = d.normalise();
-
-		// apply force to each end point
-		spring.point1.applyForce(direction.multiply(spring.k * displacement * -0.5));
-		spring.point2.applyForce(direction.multiply(spring.k * displacement * 0.5));
-	});
-};
-
-Layout.ForceDirected.prototype.attractToCentre = function() {
-	this.eachNode(function(node, point) {
-		var direction = point.p.multiply(-1.0);
-		point.applyForce(direction.multiply(this.repulsion / 50.0));
-	});
-};
-
-
-Layout.ForceDirected.prototype.updateVelocity = function(timestep) {
-	this.eachNode(function(node, point) {
-		// Is this, along with updatePosition below, the only places that your
-		// integration code exist?
-		point.v = point.v.add(point.a.multiply(timestep)).multiply(this.damping);
-		point.a = new Vector(0,0);
-	});
-};
-
-Layout.ForceDirected.prototype.updatePosition = function(timestep) {
-	this.eachNode(function(node, point) {
-		// Same question as above; along with updateVelocity, is this all of
-		// your integration code?
-		point.p = point.p.add(point.v.multiply(timestep));
-	});
-};
-
-// Calculate the total kinetic energy of the system
-Layout.ForceDirected.prototype.totalEnergy = function(timestep) {
-	var energy = 0.0;
-	this.eachNode(function(node, point) {
-		var speed = point.v.magnitude();
-		energy += 0.5 * point.m * speed * speed;
-	});
-
-	return energy;
-};
-
-
 
 
 var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }; // stolen from coffeescript, thanks jashkenas! ;-)
@@ -598,7 +593,7 @@ Layout.ForceDirected.Spring = function(point1, point2, length, k) {
         this.percX1 = 0.0;
         this.percY1 = 0.0; 
 
-    }
+    };
 
 
     mapHandler.prototype = {
